84b2428ce031d4e60d18218c6965a2905c3daddf
Donal Phipps <donal.phipps@gmail.com>
2025-12-03 20:14:30 +0000

    mobile

--- app.js
+++ app.js
@@ -949,6 +949,9 @@ class WorkoutTracker {
     this.renderExerciseInsights(exercise);
 
     this.showView("exerciseDetailView");
+    
+    // Scroll to top of page to ensure "Log Today's Session" is visible
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   showPairedExerciseDetail(exercise1, exercise2) {
